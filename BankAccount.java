9ae69aac8107dcaae1e578739338c0ad4e803f8b
Gcinanizulu <zulugcinani58@gmail.com>
2025-06-21 19:32:51 +0200

    Everything is now working what left is the README file and soem java documentation

--- BankAccount.java
+++ BankAccount.java
@@ -19,11 +19,13 @@ public class BankAccount {
             return Balance;
         } else {
             Balance -= amount;
+            System.out.printf("Successfuly withdrawn R%d. ", amount);
             return Balance;
         }
     }
     public int deposite(int amount) {
         Balance += amount;
+        System.out.printf("Successfuly deposited R%d. ", amount);
         return Balance;
     }
     
@@ -36,49 +38,78 @@ public class BankAccount {
             String creating = scanner.nextLine();
             try {
                 int option = Integer.parseInt(creating);
-                if (option == 1) {
-                    account = new BankAccount(accountInfo(scanner));
-                    bool = false;
-                } else {
+                if (option != 1) {
                     System.out.println("Thanks for your visit, hope you enjoyed it");
+                    System.exit(0);
+                } else {
+                    bool = false;
                 }
 
             } catch(Exception e) {
                 creating.toLowerCase();
-                if (creating.equals("yes")) {
-                    account = new BankAccount(accountInfo(scanner));
+                if (!creating.equals("yes")) {
+                    if (creating.equals("no")) {
+                        System.out.println("Thanks for your visit, hope you enjoyed it");
+                        System.exit(0);
+                    } else {
+                        System.err.println("Please choose one between the above options");
+                    }
+                } else {
                     bool = false;
-                }
-                else if (creating.equals("no")) {
-                    System.out.println("Thanks for your visit, hope you enjoyed it");
-                }
-                else {
-                    System.err.println("Please choose one between the above options");
                 }
             }
         }
-        
+        account = new BankAccount(accountInfo(scanner));
+        System.out.println("What can I help you wit today? ");
+        bool = true;
+        while (bool) {
+            System.out.println("1. Show Balance \n2. Withdraw from account \n3.Deposite into account \n4. Exit Application");
+            String options = scanner.nextLine();
+            switch (options) {
+                case "1":
+                    System.out.printf("Your balance R%d\n", account.Balance());
+                    break;
+                case "2":
+                    System.out.print("Amount to be withdrawn: R");
+                    int withdraw = Integer.parseInt(scanner.nextLine());
+                    account.withDraw(withdraw);
+                    System.out.printf("Your balance R%d\n", account.Balance());
+                    break;
+                case "3":
+                    System.out.print("Amount to be deposited: R");
+                    int deposite = Integer.parseInt(scanner.nextLine());
+                    account.deposite(deposite);
+                    System.out.printf("Your balance R%d\n", account.Balance());
+                    break;
+                case "4":
+                    System.out.printf("Thank you for visiting, hope you enjoyed and see you again.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.err.printf("Please choose one of the above options.");
+                    break;
+            }
+        }
     }
     private static Customer accountInfo(Scanner scanner) {
         Customer customer;
         System.out.print("First name(s): ");
         String name = scanner.nextLine();
-        //System.out.println();
 
         System.out.print("Surname name: ");
         String surname = scanner.nextLine();
-        //System.out.println();
 
-        System.out.println("Gender : \n1. Mr \n2. Mrs \n3. Miss \n4. Dr");
+        System.out.print("Gender : \n1. Mr \n2. Mrs \n3. Miss \n4. Dr");
         String gender = scanner.nextLine();
-        //System.out.println();
 
         System.out.print("Ten digit South African(+27) phone number: ");
-        int phone_number = Integer.parseInt(scanner.nextLine());
-        System.out.println();
+        String number = scanner.nextLine();
+        if (number.length() != 10) System.err.println("Please make sure that you have 10 digits for your cell number");
+        int phone_number = Integer.parseInt(number);
 
         customer = new Customer(name, surname, phone_number, gender);
-        System.out.println("Account created for %s %s %s", customer.gender, customer.surname, customer.name);
+        System.out.printf("Account created for %s %s %s\n", customer.gender, customer.Surname, customer.Name);
+        System.out.println();
         return customer;
     }
 
--- Customer.java
+++ Customer.java
@@ -1,11 +1,19 @@
 public class Customer {
     int Phone_number;
-    String Name, Surname, gender;
+    String Name, Surname,gender;
 
     public Customer(String Name, String Surname, int Phone_number, String gender) {
         this.Name = Name;
         this.Surname = Surname;
         this.Phone_number = Phone_number;
-        this.gender = gender;        
+        this.gender = gender;
+        
+        if (gender.equals("1")) this.gender = "Mr";
+        else if (gender.equals("2")) this.gender = "Mrs";
+        else if (gender.equals("4")) this.gender = "Dr";
+        else if (gender.equals("3")) this.gender = "Miss";
+        else {
+            this.gender = "NON";
+        }
     }
 }
